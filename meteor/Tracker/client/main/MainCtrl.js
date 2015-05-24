5c2f847824ac6392129e5771c1677b053b6cb1df
Mikkel Madsen <m@madsn.net>
2015-05-24 22:43:58 +0200

    Clean meteor project for Tracker

--- meteor/Tracker/client/main/MainCtrl.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('tracker.main')
-.controller('MainCtrl', function($scope){
-    console.log('Hello world');
-});
--- meteor/Tracker/mobile-config.js
+++
@@ -1,1 +0,0 @@
-App.setPreference('AutoHideSplashScreen' ,'true');
