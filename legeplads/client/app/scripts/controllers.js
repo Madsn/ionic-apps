f9acf5ba65a51719bcc139374130e9ea50a983d4
Mikkel Madsen <m@madsn.net>
2015-06-01 17:08:06 +0200

    JShint fixes

--- legeplads/client/app/scripts/controllers.js
+++ legeplads/client/app/scripts/controllers.js
@@ -1,14 +1,16 @@
+'use strict';
+
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  
+
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-  
+
   // Form data for the login modal
   $scope.loginData = {};
 
@@ -50,7 +52,8 @@ angular.module('starter.controllers', [])
     { title: 'Rap', id: 5 },
     { title: 'Cowbell', id: 6 }
   ];
-})
-
+});
+/*
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 });
+*/
